7d1c17e126861579e424a8055671f172d5a190a1
LE DOAN DAN <ledoandan2110@gmail.com>
2026-04-21 19:15:04 +0700

    Tạo project

---
+++ LDD_BT_MVC/LDD_BT_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LDD_BT_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
