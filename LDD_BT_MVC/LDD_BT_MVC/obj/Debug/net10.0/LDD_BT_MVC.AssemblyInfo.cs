a4c91511adc3950eb0f14a0749e324880f0f221e
LE DOAN DAN <ledoandan2110@gmail.com>
2026-04-21 19:46:47 +0700

    Cấu hình connection

--- LDD_BT_MVC/LDD_BT_MVC/obj/Debug/net10.0/LDD_BT_MVC.AssemblyInfo.cs
+++ LDD_BT_MVC/LDD_BT_MVC/obj/Debug/net10.0/LDD_BT_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LDD_BT_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1361aafa8ead960e56c359d8180c0315f4920b81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3da907cf6589fec879a739165a9fb9bee36db3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LDD_BT_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LDD_BT_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
